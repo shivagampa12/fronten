6ba39e727e4ad7a839ab9f41be4fcb74f10d83ce
shivagampa12 <120164119+shivagampa12@users.noreply.github.com>
2025-03-14 00:05:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const works = {
+    "task1" : {"title" : "Project 1", "url" : "works/task1/index.html"},
+    "task2" : {"title" : "Project 2", "url" : "works/task2/index.html"},
+    "task3" : {"title" : "Project 3", "url" : "works/task3/index.html"},
+};
+
+// write your code here 
+// you need to display the list of work titles in a navigation panel 
+// if a work title is clicked, the body section should display the work without reloading the page
